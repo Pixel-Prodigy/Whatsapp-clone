bce2a9a94fca50de3c45e6834eb3f200391fbb76
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-08 07:46:23 +0530

    Added layout components (Header, Left, Right)

---
+++ src/components/layout/Left.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { LeftHeader } from "../ui/left/LeftHeader";
+import { Persons } from "../ui/left/Persons";
+
+export function Left() {
+  const chatCount = 80; 
+
+  return (
+    <div className="h-full bg-[#172128] pl-4 flex flex-col">
+      <LeftHeader />
+      <div className=" flex flex-col overflow-scroll max-h-[89vh]">
+        {Array.from({ length: chatCount }).map((_, index) => (
+          <Persons key={index} />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/layout/Right.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Right() {
+  return (
+    <div className='bg-[#222e35] h-full py-4'></div>
+  )
+}
