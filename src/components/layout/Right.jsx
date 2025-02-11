cbf0da82d3bc7caef2318da94a67282aa31e4600
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-11 14:42:33 +0530

    Updated src/components/layout/Right.jsx

--- src/components/layout/Right.jsx
+++ src/components/layout/Right.jsx
@@ -3,7 +3,7 @@ import { Chat } from '../ui/right/Chat'
 
 export function Right() {
   return (
-    <div className='bg-[#222e35] h-full py-4'>
+    <div className='bg-[#222e35] h-full border-l-[1px] border-[#363e43]'>
         <Chat />
     </div>
   )
