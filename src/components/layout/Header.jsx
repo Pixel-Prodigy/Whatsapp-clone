15c1cb47fa2d5b29d497cc877a77662c015b2721
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-11 14:39:44 +0530

    Commiting all files

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { HoverBox } from "../ui/header/HoverBox";
-
+import { FaCog } from "react-icons/fa";
 export function Header() {
   const [hoverBox, setHoverBox] = useState({
     position: "absolute",
@@ -11,15 +11,15 @@ export function Header() {
   const [hoverBoxText, setHoverBoxText] = useState("");
 
   return (
-    <div className="h-full items-center bg-[#202c33] py-4 w-fit px-4 flex flex-col justify-between border-r-3 border-[#2a353e]">
-      <div className="flex flex-col gap-5 relative">
+    <div className="h-full items-center bg-[#202c33] py-4 w-[62px] flex flex-col justify-between border-r-3 border-[#2a353e]">
+      <div className="flex flex-col gap-[22px] relative">
         <HoverBox style={hoverBox} text={hoverBoxText} />
 
         <span>
           <svg
             viewBox="0 0 24 24"
-            height="25"
-            width="25"
+            height="24"
+            width="24"
             preserveAspectRatio="xMidYMid meet"
             className="text-white/60 cursor-pointer relative"
             fill="none"
@@ -50,7 +50,11 @@ export function Header() {
               clipRule="evenodd"
               d="M4.8384 8.45501L5 8.70356V9V17.8333C5 18.7538 5.7462 19.5 6.6667 19.5H20.3333C21.2538 19.5 22 18.7538 22 17.8333V6.66667C22 5.74619 21.2538 5 20.3333 5H2.5927L4.8384 8.45501ZM8 14.5C8 13.6716 8.67157 13 9.5 13H14.5C15.3284 13 16 13.6716 16 14.5C16 15.3284 15.3284 16 14.5 16H9.5C8.67157 16 8 15.3284 8 14.5ZM9.5 8C8.67157 8 8 8.67157 8 9.5C8 10.3284 8.67157 11 9.5 11H16.5C17.3284 11 18 10.3284 18 9.5C18 8.67157 17.3284 8 16.5 8H9.5Z"
               fill="currentColor"
-            ></path>
+            ></path> height="23"
+            height="23"
+            height="23"
+            height="23"
+            height="23"
             <path
               d="M5 8.70356L5.41919 8.43101L5.5 8.55531V8.70356H5ZM4.8384 8.45501L4.41921 8.72756L4.41917 8.7275L4.8384 8.45501ZM2.5927 5L2.17347 5.27249L1.67137 4.5H2.5927V5ZM4.58081 8.9761L4.41921 8.72756L5.25759 8.18247L5.41919 8.43101L4.58081 8.9761ZM4.5 9V8.70356H5.5V9H4.5ZM4.5 17.8333V9H5.5V17.8333H4.5ZM6.6667 20C5.47006 20 4.5 19.0299 4.5 17.8333H5.5C5.5 18.4777 6.02234 19 6.6667 19V20ZM20.3333 20H6.6667V19H20.3333V20ZM22.5 17.8333C22.5 19.0299 21.53 20 20.3333 20V19C20.9777 19 21.5 18.4777 21.5 17.8333H22.5ZM22.5 6.66667V17.8333H21.5V6.66667H22.5ZM20.3333 4.5C21.53 4.5 22.5 5.47005 22.5 6.66667H21.5C21.5 6.02233 20.9777 5.5 20.3333 5.5V4.5ZM2.5927 4.5H20.3333V5.5H2.5927V4.5ZM4.41917 8.7275L2.17347 5.27249L3.01192 4.72751L5.25762 8.18252L4.41917 8.7275ZM9.5 13.5C8.94772 13.5 8.5 13.9477 8.5 14.5H7.5C7.5 13.3954 8.39543 12.5 9.5 12.5V13.5ZM14.5 13.5H9.5V12.5H14.5V13.5ZM15.5 14.5C15.5 13.9477 15.0523 13.5 14.5 13.5V12.5C15.6046 12.5 16.5 13.3954 16.5 14.5H15.5ZM14.5 15.5C15.0523 15.5 15.5 15.0523 15.5 14.5H16.5C16.5 15.6046 15.6046 16.5 14.5 16.5V15.5ZM9.5 15.5H14.5V16.5H9.5V15.5ZM8.5 14.5C8.5 15.0523 8.94772 15.5 9.5 15.5V16.5C8.39543 16.5 7.5 15.6046 7.5 14.5H8.5ZM7.5 9.5C7.5 8.39543 8.39543 7.5 9.5 7.5V8.5C8.94772 8.5 8.5 8.94772 8.5 9.5H7.5ZM9.5 11.5C8.39543 11.5 7.5 10.6046 7.5 9.5H8.5C8.5 10.0523 8.94772 10.5 9.5 10.5V11.5ZM16.5 11.5H9.5V10.5H16.5V11.5ZM18.5 9.5C18.5 10.6046 17.6046 11.5 16.5 11.5V10.5C17.0523 10.5 17.5 10.0523 17.5 9.5H18.5ZM16.5 7.5C17.6046 7.5 18.5 8.39543 18.5 9.5H17.5C17.5 8.94772 17.0523 8.5 16.5 8.5V7.5ZM9.5 7.5H16.5V8.5H9.5V7.5Z"
               fill="currentColor"
@@ -60,8 +64,8 @@ export function Header() {
         <span>
           <svg
             viewBox="0 0 24 24"
-            height="25"
-            width="25"
+            height="24"
+            width="24"
             preserveAspectRatio="xMidYMid meet"
             className="text-white/60 cursor-pointer relative"
             fill="none"
@@ -110,8 +114,8 @@ export function Header() {
         <span>
           <svg
             viewBox="0 0 24 24"
-            height="25"
-            width="25"
+            height="24"
+            width="24"
             preserveAspectRatio="xMidYMid meet"
             className="text-white/60 cursor-pointer relative"
             fill="none"
@@ -158,8 +162,8 @@ export function Header() {
         <span>
           <svg
             viewBox="0 0 24 24"
-            height="25"
-            width="25"
+            height="24"
+            width="24"
             preserveAspectRatio="xMidYMid meet"
             className="text-white/60 cursor-pointer relative"
             fill="none"
@@ -244,12 +248,12 @@ export function Header() {
           </svg>
         </span>
       </div>
-      <div className=" flex flex-col gap-7">
+      <div className=" flex flex-col gap-5 items-center">
         <span>
           <svg
             viewBox="0 0 24 24"
-            height="25"
-            width="25"
+            height="24"
+            width="24"
             preserveAspectRatio="xMidYMid meet"
             className="text-white/60 cursor-pointer relative"
             fill="none"
@@ -286,9 +290,8 @@ export function Header() {
         <span>
           <img
             tabindex="-1"
-            width="28"
-            height="28"
-            className="bg-transparent"
+       
+            className="h-[32px] min-w-[32px]  rounded-full"
             src="https://media-del1-2.cdn.whatsapp.net/v/t61.24694-24/380986083_1310618473176295_1585446783264938143_n.jpg?stp=dst-jpg_s96x96_tt6&amp;ccb=11-4&amp;oh=01_Q5AaIE1ZFwps5FUak-w6XteuV-buhVs1I-ALkyFueUGJCgML&amp;oe=67B3A230&amp;_nc_sid=5e03e0&amp;_nc_cat=100"
           />
         </span>
