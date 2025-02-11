8424a27aa87b5f12f04ecaad8614093110c3c8a8
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-11 14:42:33 +0530

    Updated src/components/layout/Left.jsx

--- src/components/layout/Left.jsx
+++ src/components/layout/Left.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { LeftHeader } from "../ui/left/LeftHeader";
 import { Persons } from "../ui/left/Persons";
 
-export function Left({children}) {
-  const chatCount = 80; 
-
+export function Left({ children,}) {
+  const  chatCount = 50 
   return (
-    <div className="h-full bg-[#172128] pl-4 flex flex-col">
-      <LeftHeader name={'Chats'} />
-      <div className=" flex flex-col overflow-scroll max-h-[80vh]  ">
-        {Array.from({ length: chatCount }).map((_, index) => (
+    <div className="h-full bg-[#111b21] flex flex-col">
+      <LeftHeader name="Chats" />
+      <div className="scrollbar  flex flex-col  overflow-y-auto flex-1 max-h-[calc(100vh-153px)] ">
+        {Array.from({length: chatCount}).map((_, index) => (
           <Persons key={index} />
         ))}
       </div>
